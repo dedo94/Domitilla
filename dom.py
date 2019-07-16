28f4455fdb75acf6d4857afcea02fbb7cf85795b
Davide <davideschiavi94@gmail.com>
2019-07-16 11:04:23 +0200

    Add files via upload

--- dom.py
+++ dom.py
@@ -62,6 +62,10 @@ class MyBtnBar(BoxLayout):
 
 class MyLayout(BoxLayout):
     # print(kivy.__version__)
+    def errpop(self):
+        err = Popup(title="OOPS...", content=Label(text='Sorry, something went wrong...\n\nClick outside for dismiss.'),
+                    size_hint=(None, None), size=(400, 400))
+        err.open()
 
     def pathfy(self):
         path_file = open("path.txt", 'r')
@@ -87,7 +91,8 @@ class MyLayout(BoxLayout):
         self.ids.gr_2.color = 51, 51, 51, 1
         self.ids.gr_1.save_path = "folder"
         self.ids.gr_2.save_path = 51, 51, 51, 1
-        self.ids.fuse_area.text = "Insert each pair of nodes in a different row.\nExample:\nA -> H : m ; K -> B : m"
+        self.ids.fuse_area.text = "Insert each pair of nodes in a different row.\nExample 1:\n" \
+                                  "A -> H : n ; K -> B : n\nExample 2:\n[A,B]"
         self.ids.save_name.text = "prova"
 
     def load(self, path, selection):
@@ -239,147 +244,163 @@ class MyLayout(BoxLayout):
         self.overwrite_path()
 
     def fuse(self):
-        name1 = self.ids.gr_1.text
-        name2 = self.ids.gr_2.text
-        node_str = self.ids.fuse_area.text
-        nf = []                                                                                                         # lista con le coppie di label da fondere
-        part = []                                                                                                       # lista che conterrà i partecipanti
-        method1 = False
-        method2 = False
-        nfline = node_str.split("\n")
+        try:
+            name1 = self.ids.gr_1.text
+            name2 = self.ids.gr_2.text
+            node_str = self.ids.fuse_area.text
+            nf = []                                                                                                     # lista con le coppie di label da fondere
+            part = []                                                                                                   # lista che conterrà i partecipanti
+            method1 = False
+            method2 = False
+            nfline = node_str.split("\n")
 
-        for el in range(nfline.__len__()):
+            for el in range(nfline.__len__()):
 
-            if nfline[el].count(";"):
-                method1 = True
-                couple = nfline[el].split(";")
-                a = couple[0].strip()                                                                                   # primo label
-                b = couple[1].strip()                                                                                   # secondo label
-                if a.count("->") == b.count("->") == 1 and a.count(":") == b.count(":") == 1:
-                    cpl = [a, b]                                                                                        # coppia dei label
-                    nf.append(cpl)                                                                                      # la aggiungo alla lista
+                if nfline[el].count(";"):
+                    method1 = True
+                    couple = nfline[el].split(";")
+                    a = couple[0].strip()                                                                               # primo label
+                    b = couple[1].strip()                                                                               # secondo label
+                    if a.count("->") == b.count("->") == 1 and a.count(":") == b.count(":") == 1:
+                        cpl = [a, b]                                                                                    # coppia dei label
+                        nf.append(cpl)                                                                                  # la aggiungo alla lista
 
-            if nfline[el].count("[") == nfline[el].count(",") == nfline[el].count("]") == 1:
-                method2 = True
-                half = nfline[el].split(",")
-                half1 = half[0].strip("[")
-                half1 = half1.strip()
-                part.append(half1)
-                half2 = half[1].strip("]")
-                half2 = half2.strip()
-                part.append(half2)
+                if nfline[el].count("[") == nfline[el].count(",") == nfline[el].count("]") == 1:
+                    method2 = True
+                    half = nfline[el].split(",")
+                    half1 = half[0].strip("[")
+                    half1 = half1.strip()
+                    part.append(half1)
+                    half2 = half[1].strip("]")
+                    half2 = half2.strip()
+                    part.append(half2)
 
-        if method2 is True and method1 is True:
-            print("use only one method")
-
-        else:
-
-            if name1.find(".gv") == name1.__len__() - 3 and name2.find(".gv") == name2.__len__() - 3:
-                comp = composition(name1, name2, self.ids.save_name.text, 0)
-                if method1 is True:
-                    refusion(comp, self.ids.save_name.text, nf, 1)
-
-                elif method2 is True:
-                    cp_list = fuseplus(comp, part[0], part[1])
-                    refusion(comp, self.ids.save_name.text, cp_list, 1)
-
-            elif name1.find(".gv") == name1.__len__() - 3:
-                if name2 == "file2" or name2 == "file 2 missing":
-                    a1 = []
-                    draw1 = name1.split("/")
-                    draw1 = draw1[-1].split(".")
-                    dot_not_struct(name1, draw1[0] + ".gv", a1, 0)
-
-                    if method1:
-                            refusion(a1, self.ids.save_name.text, nf, 1)
-
-                    elif method2:
-                        cp_list = fuseplus(a1, part[0], part[1])
-                        refusion(a1, self.ids.save_name.text, cp_list, 1)
-
-            elif name2.find(".gv") == name2.__len__() - 3:
-                if name1 == "file1" or name1 == "file 1 missing":
-                    a2 = []
-                    draw2 = name2.split("/")
-                    draw2 = draw2[-1].split(".")
-                    dot_not_struct(name2, draw2[0] + ".gv", a2, 0)
-                    if method1:
-                        refusion(a2, self.ids.save_name.text, nf, 1)
-
-                    elif method2:
-                        cp_list = fuseplus(a2, part[0], part[1])
-                        refusion(a2, self.ids.save_name.text, cp_list, 1)
+            if method2 is True and method1 is True:
+                popupz = Popup(title="Synchronization methods",
+                               content=Label(text='Two different Synchronization methods detected, please use only one.'
+                                                  '\n\nClick outside for dismiss.'), size_hint=(None, None),
+                               size=(1000, 400))
+                popupz.open()
 
             else:
-                if name1 == "file1":
-                    self.ids.gr_1.text = "select Unstructured graph"
-                    self.ids.gr_1.color = 125, 0, 0, 1
-                elif name1.find(".gv") < 0:
-                    self.ids.gr_1.text = "Unsupported file"
-                    self.ids.gr_1.color = 125, 0, 0, 1
-                if name2 == "file2":
-                    self.ids.gr_2.text = "select Unstructured graph"
-                    self.ids.gr_2.color = 125, 0, 0, 1
-                elif name2.find(".gv") < 0:
-                    self.ids.gr_2.text = "Unsupported file"
-                    self.ids.gr_2.color = 125, 0, 0, 1
 
-        self.overwrite_path()
+                if name1.find(".gv") == name1.__len__() - 3 and name2.find(".gv") == name2.__len__() - 3:
+                    comp = composition(name1, name2, self.ids.save_name.text, 0)
+                    if method1 is True:
+                        refusion(comp, self.ids.save_name.text, nf, 1)
+
+                    elif method2 is True:
+                        cp_list = fuseplus(comp, part[0], part[1])
+                        refusion(comp, self.ids.save_name.text, cp_list, 1)
+
+                elif name1.find(".gv") == name1.__len__() - 3:
+                    if name2 == "file2" or name2 == "file 2 missing":
+                        a1 = []
+                        draw1 = name1.split("/")
+                        draw1 = draw1[-1].split(".")
+                        dot_not_struct(name1, draw1[0] + ".gv", a1, 0)
+
+                        if method1:
+                                refusion(a1, self.ids.save_name.text, nf, 1)
+
+                        elif method2:
+                            cp_list = fuseplus(a1, part[0], part[1])
+                            refusion(a1, self.ids.save_name.text, cp_list, 1)
+
+                elif name2.find(".gv") == name2.__len__() - 3:
+                    if name1 == "file1" or name1 == "file 1 missing":
+                        a2 = []
+                        draw2 = name2.split("/")
+                        draw2 = draw2[-1].split(".")
+                        dot_not_struct(name2, draw2[0] + ".gv", a2, 0)
+                        if method1:
+                            refusion(a2, self.ids.save_name.text, nf, 1)
+
+                        elif method2:
+                            cp_list = fuseplus(a2, part[0], part[1])
+                            refusion(a2, self.ids.save_name.text, cp_list, 1)
+
+                else:
+                    if name1 == "file1":
+                        self.ids.gr_1.text = "select Unstructured graph"
+                        self.ids.gr_1.color = 125, 0, 0, 1
+                    elif name1.find(".gv") < 0:
+                        self.ids.gr_1.text = "Unsupported file"
+                        self.ids.gr_1.color = 125, 0, 0, 1
+                    if name2 == "file2":
+                        self.ids.gr_2.text = "select Unstructured graph"
+                        self.ids.gr_2.color = 125, 0, 0, 1
+                    elif name2.find(".gv") < 0:
+                        self.ids.gr_2.text = "Unsupported file"
+                        self.ids.gr_2.color = 125, 0, 0, 1
+
+            self.overwrite_path()
+
+        except:
+            self.errpop()
 
     def petri(self):
-        path_file = open("path.txt", 'r')
-        paths = []
-        for line in path_file:
-            paths.append(line.strip())
-        path_file.close()
-        # per Windows
-        if platform.system() == "Windows":
-            pass
-        # per macOs
-        if platform.system() == "Darwin":
-            path = paths[0]
-        # per Linux
-        if platform.system() == "Linux":
-            path = paths[1]
+        try:
+            path_file = open("path.txt", 'r')
+            paths = []
+            for line in path_file:
+                paths.append(line.strip())
+            path_file.close()
+            # per Windows
+            if platform.system() == "Windows":
+                pass
+            # per macOs
+            if platform.system() == "Darwin":
+                path = paths[0]
+            # per Linux
+            if platform.system() == "Linux":
+                path = paths[1]
 
-        name1 = self.ids.gr_1.text
-        namea = name1.split("/")
-        namea = namea[-1].split(".")
-        namea = namea[0]
-        a = []
-        if name1.find(".gv") == name1.__len__() - 3:
-            dot_not_struct(name1, namea + ".gv", a, 0)
-            petri2(name1, namea, a, path)
-        elif name1.find(".txt") == name1.__len__() - 4:
-            struct_gr(name1, namea, a, 0)
-            petri2(name1, namea, a, path)
-        elif name1 == "file1" or name1 == "file 1 missing" or \
-                name1 == "Unsupported file" or name1 == "select Unstructured graph":
-            self.ids.gr_1.text = "file1"
-            self.ids.gr_1.color = 51, 51, 51, 1
-        else:
-            self.ids.gr_1.text = "Unsupported file"
-            self.ids.gr_1.color = 125, 0, 0, 1
+            name1 = self.ids.gr_1.text
+            namea = name1.split("/")
+            namea = namea[-1].split(".")
+            namea = namea[0]
+            a = []
+            if name1.find(".gv") == name1.__len__() - 3:
+                dot_not_struct(name1, namea + ".gv", a, 0)
+                petri2(name1, namea, a, path)
+            elif name1.find(".txt") == name1.__len__() - 4:
+                struct_gr(name1, namea, a, 0)
+                petri2(name1, namea, a, path)
+            elif name1 == "file1" or name1 == "file 1 missing" or \
+                    name1 == "Unsupported file" or name1 == "select Unstructured graph":
+                self.ids.gr_1.text = "file1"
+                self.ids.gr_1.color = 51, 51, 51, 1
+            else:
+                self.ids.gr_1.text = "Unsupported file"
+                self.ids.gr_1.color = 125, 0, 0, 1
 
-        name2 = self.ids.gr_2.text
-        nameb = name2.split("/")
-        nameb = nameb[-1].split(".")
-        nameb = nameb[0]
-        b = []
-        if name2.find(".gv") == name2.__len__() - 3:
-            petri2(name2, nameb, b, path)
-        elif name2.find(".txt") == name2.__len__() - 4:
-            struct_gr(name2, nameb, b, 0)
-            petri2(name2, nameb, b, path)
-        elif name2 == "file2" or name2 == "file 2 missing" or \
-                name2 == "Unsupported file" or name2 == "select Unstructured graph":
-            self.ids.gr_2.text = "file2"
-            self.ids.gr_2.color = 51, 51, 51, 1
-        else:
-            self.ids.gr_2.text = "Unsupported file"
-            self.ids.gr_2.color = 125, 0, 0, 1
+            name2 = self.ids.gr_2.text
+            nameb = name2.split("/")
+            nameb = nameb[-1].split(".")
+            nameb = nameb[0]
+            b = []
+            if name2.find(".gv") == name2.__len__() - 3:
+                petri2(name2, nameb, b, path)
+            elif name2.find(".txt") == name2.__len__() - 4:
+                struct_gr(name2, nameb, b, 0)
+                petri2(name2, nameb, b, path)
+            elif name2 == "file2" or name2 == "file 2 missing" or \
+                    name2 == "Unsupported file" or name2 == "select Unstructured graph":
+                self.ids.gr_2.text = "file2"
+                self.ids.gr_2.color = 51, 51, 51, 1
+            else:
+                self.ids.gr_2.text = "Unsupported file"
+                self.ids.gr_2.color = 125, 0, 0, 1
 
-        self.overwrite_path()
+            self.overwrite_path()
+
+        except:
+            popupz = Popup(title="Translation error",
+                           content=Label(text='For some reasons translation is unavailable at the moment.'
+                                              '\n\nClick outside for dismiss.'),
+                           size_hint=(None, None), size=(1000, 400))
+            popupz.open()
 
     def help(self):
         file = "guide.pdf"
